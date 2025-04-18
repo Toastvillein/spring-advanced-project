25def3760df0d35f369232570a4308c418da36cf
Toastvillein <okkcba7899@gmail.com>
2025-04-18 17:00:11 +0900

    level 2: N+1

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +12,18 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    // Fetch Join 중에 페이징은 메모리를 사용하기 때문에 Entity Graph로 변경
+//    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+
+
+    // @EntityGraph(attributePaths = {"user"})
+    // @Query("SELECT t FROM Todo t WHERE t.id = :todoId")
+    // 아래 JPQL은 위처럼 바꿀 수 있으나 굳이 바꿔야할 이유를 모르겠어서 그냥 냅둠
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
