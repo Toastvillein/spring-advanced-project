c4e8612ed336a6b633df31963aeeed398cce2add
Toastvillein <okkcba7899@gmail.com>
2025-04-20 03:23:40 +0900

    level 5: fix domain.auth.dto.request SignupRequest Validation

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,8 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,9 @@ public class SignupRequest {
     @NotBlank @Email
     private String email;
     @NotBlank
+    // lv5 문제해결 과제
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$",message = "최소 한 개의 숫자와 대문자를 포함해야 합니다.")
     private String password;
     @NotBlank
     private String userRole;
